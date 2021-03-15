eeb0e45787454164c2ea30bd36c663c4587df875
Ruslan <vydyvnyk@gmail.com>
2021-03-15 19:17:20 +0200

    3 day

--- js/main.js
+++ js/main.js
@@ -5,7 +5,15 @@ $(function () {
         dots: true,
         autoplay: true,
         fade: true,
-
+        /* прибираю дотси менше 1200px */
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    dots: false
+                }
+            },
+        ]
     });
 
     $('.reviews__slider').slick({
@@ -13,6 +21,33 @@ $(function () {
         dots: true,
         slidesToShow: 4,
         slidesToScroll: 1,
+        responsive: [    /* зменшуєм к-ть відг. при 1140px */
+            {
+                breakpoint: 1141,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+
+            {             /* зменшуєм к-ть відг. при 845px */
+                breakpoint: 846,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+
+            {             /* зменшуєм к-ть відг. при 585px */
+                breakpoint: 586,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+
+        ]
+    });
+
+    $('.menu__btn').on('click', function () {
+        $('.menu__list').toggleClass('menu__list--active');
     });
 
 });
